a96aad8e5216346eacf9b83b3bc0b5985a67930b
Dan Karlin <dan@ciems.org>
2025-10-23 16:14:46 -0500

    Initial Commit

---
+++ ElTrack/ElTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElTrackApp.swift
+//  ElTrack
+//
+//  Created by Daniel Karlin on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ElTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
